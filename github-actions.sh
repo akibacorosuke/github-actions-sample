a6bc7ddbc4741d91b61f87285ab1b237d40f4f14
Daichi Akiba <32122466+akibacorosuke@users.noreply.github.com>
2022-08-04 00:28:09 +0900

    Create github-actions.sh

---
+++ github-actions.sh
@@ -0,0 +1,21 @@
+          echo Add other actions to build,
+          echo test, and deploy your project.
+          echo Is maven installed?
+          mvn --version
+          echo Is jave installed?
+          java -version
+          echo Is gradle installed?
+          gradle --version
+          echo ==Python==
+          python --version
+          echo ==Node==
+          node --version
+          ls -a
+          echo pwd
+          pwd
+          echo env
+          env
+          echo selenium jars
+          echo $SELENIUM_JAR_PATH
+          echo Android SDK root
+          echo $ANDROID_SDK_ROOT
